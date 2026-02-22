5b1cc6db01c342fd0f8580b5e598d9b842ab8fd6
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 19:12:28 +0000

    7th code

---
+++ q7.java
@@ -0,0 +1,10 @@
+class q7{
+    public static void main(String[] args){
+        int i=1;
+        while(i<11){
+            int mul=8*i;
+            System.out.println("8 X "+i+"="+mul);
+            i++;
+        }
+    }
+}
