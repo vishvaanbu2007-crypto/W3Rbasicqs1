1c015974085c9037cbccd96ab1941773b6d80413
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 18:13:18 +0000

    added q3

---
+++ q3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class q3{
+    public static void main(String[] args){
+        double a,b,div;
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter value of a: ");
+        a=s.nextDouble();
+        System.out.println("enter value of b");
+        b=s.nextDouble();
+        div=a/b;
+        System.out.println("The division of a/b is: "+div);
+        
+    }
+
+}
+
