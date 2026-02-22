198964de78813a538f7a6a921761e3c4e46b478d
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 19:35:13 +0000

    q13

---
+++ q13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class q13 {
+ public static void main(String[] args){
+    double l,b,a;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter l : ");
+    l=s.nextDouble();
+    System.out.println("enter b : ");
+    b=s.nextDouble();
+    a=l*b;
+    System.out.println("Area of rectangle is: "+a);
+    
+ }   
+}
