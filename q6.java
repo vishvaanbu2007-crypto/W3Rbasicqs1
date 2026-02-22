be343558d7415d51cafc01a11fbfa925d11e13bc
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 19:06:21 +0000

    6thcode

---
+++ q6.java
@@ -0,0 +1,15 @@
+class q6{
+    public static void main(String[] args){
+        double n1=125,n2=24,a1,a2,a3,a4,a5;
+        a1=n1+n2;
+        a2=n1-n2;
+        a3=n1*n2;
+        a4=n1/n2;
+        a5=n1%n2;
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+    }
+}
