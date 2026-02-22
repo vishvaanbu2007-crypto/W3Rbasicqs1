6e7f874a957d6c82a79f46bb3d6722373fdd00fb
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 18:32:11 +0000

    4th code

---
+++ q4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class q4{
+    public static void main(String[] args){
+        double a1,a2,a3,a4;
+        a1=-5+8*6;
+        a2=(55+9)%9;
+        a3=20+(-3)*5/8;
+        a4=5+15/3*2-8%3;
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+    }
+}
