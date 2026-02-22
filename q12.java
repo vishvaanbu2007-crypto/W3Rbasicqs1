9d9d45ae50ac5da43179ef9dd08c4048b9bdddfb
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 19:29:58 +0000

    12th code

---
+++ q12.java
@@ -0,0 +1,8 @@
+class q12 {
+    public static void main(String[] args){
+double a=2,s=3,d=5,avg;
+avg=(a+s+d)/3;
+System.out.println(avg);
+
+    }
+}
---
+++ q9.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class q9 {
+ public static void main(String[] args){
+    double a;
+    a=((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    System.out.println(a);
+ }   
+}
