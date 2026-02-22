0775cbf1d514f962b876bf13b1515b45efdc7d22
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-02-22 18:56:17 +0000

    5th code

---
+++ q5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class q5 {
+    public static void main(String[] args){
+        double a,b,mul;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a=s.nextDouble();
+        System.out.println("Enter b: ");
+        b=s.nextDouble();
+        mul=a*b;
+        System.out.println(mul);
+
+
+    }
+    
+}
